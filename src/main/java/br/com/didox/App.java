5a6944a46649ce2ae751dcaa1435d3469b824ba6
Lucas Pina <ti.lucaspina@gmail.com>
2023-07-23 20:26:28 -0300

    exercicio basico

--- src/main/java/br/com/didox/App.java
+++ src/main/java/br/com/didox/App.java
@@ -1,11 +1,25 @@
 package br.com.didox;
 
+import java.util.Scanner;
+
 /**
  * Hello world!
  *
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Informe o valor de X");
+        int x = sc.nextInt();
+        System.out.println("Informe o valor de Y");
+        int y = sc.nextInt();
+
+        int result = (x * 8 + y) - x + y / 100;
+
+        System.out.println("O resultado é: " + result);
+
+        sc.close();
+
     }
 }
